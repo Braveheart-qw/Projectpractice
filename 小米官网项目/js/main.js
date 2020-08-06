a8959ce10429f912a93e1219a19f9f3ca9aed917
Braveheart-qw <609830076@qq.com>
2020-08-06 22:05:57 +0800

    小米官网

--- 小米官网项目/js/main.js
+++ 小米官网项目/js/main.js
@@ -5,9 +5,16 @@ require.config({
     paths:{
         jquery : "../js/jquery-3.5.1.min",
         index_animate: "../js/index_animate",
-        move : '../js/btn_move'
+        move : '../js/btn_move',
+
+    },
+    shim:{
+        bootstrap_bundle_js: ['jquery'],
+        bootstrap_min_js: ['jquery']
     }
 })
+
+
 
 require(['index_animate' ] , function (index_animate) {
        require(['move'] , function (move) {
